de1a8fb98682478c0a7acef6613536a390740ca1
George Vega Yon <g.vegayon@gmail.com>
2013-04-21 10:10:57 -0300

    Writing data

--- R/dataProccess.R
+++ R/dataProccess.R
@@ -1,4 +1,8 @@
 readPDStable <- function(dataid) {
+  
+  # Stuff
+  dataid <- gsub("\\.[0-9]+$","",tolower(dataid))
+  
   # Reading files
   lbl <- parseLbl(readLines(paste(dataid, "lbl",sep=".")))
   tab <- read.table(paste(dataid, "tab",sep="."), skip=2)
@@ -58,5 +62,48 @@ summary.PDS <- function(object, ...) {
   return(result)
 }
 
+downLoadMission <- function(datatree, maxdata=-1) {
+  sources <- names(datatree)
+  
+  for (i in 1:length(sources)) {
+    d <- datatree[[i]]
+    
+    # Test if there are tab or lbl files
+    test <- grepl("(\\.tab$|\\.lbl$)",d)
+    
+    if (any(test)) {
+      # Subseting
+      dsub <- d[test]
+      dsub2 <- unique(gsub("\\.tab^|\\.lbl", "", dsub))
+      
+      # Start parsing 
+      for (j in 1:length(dsub2)) {
+        
+        # Subsets lbl and tab of one file
+        ftpurllbl <- sprintf("%s/%s.lbl", sources[i], dsub2[j])
+        ftpurltab <- sprintf("%s/%s.tab", sources[i], dsub2[j])
+        
+        message("Conecting to ",ftpurllbl," ...")
+        lbl <- try(getURL(ftpurllbl), silent=TRUE)
+        
+        if (class(lbl) != "try-error") {
+          message("Conecting to ",ftpurltab," ...")
+          tab <- try(getURL(ftpurltab), silent=TRUE)
+        }
+        else next
+        
+        if (class(tab) != "try-error") {
+          # Writing data
+          message("Writing tables at ",getwd())
+          writeLines(lbl, sprintf("%s.lbl", dsub2[j]))
+          writeLines(tab, sprintf("%s.tab", dsub2[j]))
+        }
+        else next        
+      }
+      
+    }
+  }
+}
 #x <- readPDStable(header="data/apollo12_sws_1hr_1976c9388")
 #summary(x, nvars=2)
+
--- demo/apollo17.R
+++ demo/apollo17.R
@@ -4,3 +4,9 @@ apollo <- dirMissions("apollo 17")
 
 # What data is there
 apollo.data <- exploreMission(apollo[1,1])
+
+# Download the data
+downLoadMission(apollo.data)
+
+# Read data
+apollo.df <- readPDStable("a17_traverse_gravity_data")
